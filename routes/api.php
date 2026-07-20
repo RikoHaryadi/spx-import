66db03a175eceb3a73550bd36e85684e7debeadf
Riko <rikoharyadi0908@gmail.com>
2026-07-20 23:00:08 +0700

    Add Google Sheet API Import

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\GoogleSheetController;
 
-Route::post('/suite/import',[GoogleSheetController::class,'importSuite']);
-Route::post('/tracking/import',[GoogleSheetController::class,'importTracking']);
+Route::post('/suite/import', [GoogleSheetController::class, 'importSuite']);
+Route::post('/tracking/import', [GoogleSheetController::class, 'importTracking']);
