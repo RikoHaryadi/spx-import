98e0bf51cc4e7a21e1bcaa0ba7f44ca01142dbf5
Riko <rikoharyadi0908@gmail.com>
2026-08-04 11:43:13 +0700

    Fix monitoring_tracking migration

--- database/migrations/2026_08_04_043505_create_monitoring_tracking_table.php
+++ database/migrations/2026_08_04_043505_create_monitoring_tracking_table.php
@@ -10,12 +10,50 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('monitoring_tracking', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('monitoring_tracking', function (Blueprint $table) {
+
+        $table->id();
+
+        $table->string('order_id',100)->unique();
+
+        $table->string('driver_id',50)->nullable()->index();
+        $table->string('driver_name',255)->nullable()->index();
+
+        $table->string('initial_driver_id',50)->nullable();
+        $table->string('initial_driver_name',255)->nullable();
+
+        $table->string('current_driver_id',50)->nullable();
+        $table->string('current_driver_name',255)->nullable();
+
+        $table->foreignId('hub_id')
+            ->nullable()
+            ->constrained('hubs')
+            ->nullOnDelete();
+
+        $table->dateTime('received_time')->nullable();
+        $table->dateTime('current_station_received_time')->nullable();
+        $table->dateTime('delivering_time')->nullable();
+        $table->dateTime('delivered_time')->nullable();
+        $table->dateTime('on_hold_time')->nullable();
+
+        $table->string('on_hold_reason',255)->nullable();
+
+        $table->dateTime('reschedule_date')->nullable();
+
+        $table->string('status',100)->nullable()->index();
+
+        $table->date('operation_date')->nullable()->index();
+
+        $table->string('order_account',255)->nullable();
+
+        $table->string('payment_method',100)->nullable();
+
+        $table->string('current_station',255)->nullable();
+
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
